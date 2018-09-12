836cefe566baa47a22087a3067f04a1c0a548d72
Martin Sumner <martin.sumner@adaptip.co.uk>
2018-09-12 10:47:08 +0100

    Initial test site

---
+++ plates.py
@@ -0,0 +1,51 @@
+import csv
+import random
+import re
+
+CSV_FLDR = 'csv'
+ALPHA_RE = re.compile(r"[A-Z]+")
+
+class Plates():
+
+    def __init__(self, dealers):
+        self.dealers = dealers
+        self.plates = []
+        for (dealer, vat_status) in dealers:
+            with open (CSV_FLDR + "/" + dealer + ".csv", 'rb') as csvFile:
+                reader = csv.reader(csvFile)
+                for [plate, price] in reader:
+                    searchPlate = plate.replace(" ", "") 
+                    plateT = (searchPlate, plate, price, dealer, vat_status)
+                    self.plates.append(plateT)
+    
+    def match_plate(self, matchString):
+        # First try and find up to three letters in the search string
+        # Want to match on letters only, if letters are provided, ignoring
+        # any other letters after the first 3
+        # If no letters provided match on the whole search string
+        alphaMatches = ALPHA_RE.match(matchString)
+        if alphaMatches:
+            refinedMatch = alphaMatches.group(0)[:3]
+        else:
+            refinedMatch = matchString
+
+        beginRe = re.compile(r".*\b" + refinedMatch)
+        matchRe = re.compile(r".*" + refinedMatch)
+        beginResults, matchResults = [], []
+        for plateT in self.plates:
+            if beginRe.match(plateT[1]):
+                beginResults.append(plateT)
+            elif matchRe.match(plateT[0]):
+                matchResults.append(plateT)
+        return beginResults + matchResults
+    
+    def alphabet_list(self, alpha):
+        # Only interested in plates beginning with a word
+        beginRe = re.compile(r".*\b" + alpha)
+        beginResults = []
+        for plateT in self.plates:
+            if beginRe.match(plateT[1]):
+                beginResults.append(plateT)
+        return beginResults
+
+
---
+++ simple.py
@@ -0,0 +1,73 @@
+import tornado.ioloop
+import tornado.web
+import plates
+import os
+import re
+
+LIST_PARAM = re.compile(r"^[A-Z]+$")
+SEARCH_PARAM = re.compile(r"^[A-Za-z0-9]+$")
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def get_template_path(self):
+        dirname = os.path.dirname(__file__)
+        return os.path.join(dirname, "templates")
+
+    def get(self):
+        self.render("index.html")
+
+class RegistrationHandler(MainHandler):
+
+    def initialize(self, dealer_plates):
+        self.dealer_plates = dealer_plates
+    
+    def validate_regex(self, regex, param):
+        if regex.match(param):
+            return True
+        else:
+            return False
+
+    def invalid_input(self):
+        self.render("invalid_input.html")
+
+class SearchHandler(RegistrationHandler):
+
+    def get(self):
+        searchStr = self.get_argument("search")
+        if self.validate_regex(SEARCH_PARAM, searchStr):
+            results = self.dealer_plates.match_plate(searchStr.upper())
+            self.render("search_results.html", 
+                        result_count=len(results), 
+                        search_term=searchStr.upper(), 
+                        results=results)
+        else:
+            self.invalid_input()
+
+class ListHandler(RegistrationHandler):
+
+    def get(self):
+        beginsStr = self.get_argument("begins")
+        if self.validate_regex(LIST_PARAM, beginsStr):
+            results = self.dealer_plates.alphabet_list(beginsStr.upper())
+            self.render("list_results.html", 
+                        result_count=len(results), 
+                        alphabet_term=beginsStr.upper(), 
+                        results=results)
+        else:
+            self.invalid_input()
+        
+
+
+def make_app():
+    dealers = [("dealer1", True), ("dealer2", False)]
+    dealer_plates = plates.Plates(dealers)
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/search", SearchHandler, dict(dealer_plates = dealer_plates)),
+        (r"/list", ListHandler, dict(dealer_plates = dealer_plates)),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
